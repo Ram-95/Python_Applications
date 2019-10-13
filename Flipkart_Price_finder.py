1d53c2025fa7f606d58b452275b70643904f5008
Ram Babu <ram.kottapally@gmail.com>
2019-10-13 18:29:59 +0530

    Add files via upload

--- Flipkart_Price_finder.py
+++ Flipkart_Price_finder.py
@@ -1,23 +1,27 @@
-# Flipkart Items Price finder
+# Flipkart Items Price finder - Prints the price of the item from the URL given as arguments.
 
+#Necessary Modules
 import bs4 as bs
 import urllib.request
 
 
-def flipkart_price(url):  
+def flipkart_price(url):
+    ''' Prints the price of the Items' URLs provided as argument.'''
     source = urllib.request.urlopen(url).read()
     soup = bs.BeautifulSoup(source, 'lxml')
 
-    #getting the name of the item
+    #Getting the name of the item
     item_name = soup.find('p').text
 
-    #getting the price of item
+    #Getting the price of item
     price = soup.find('div', class_ = '_1vC4OE _3qQ9m1').text
 
     print("Current price of \"{}\" is: Rs. {}/-".format(item_name, price))
 
-
-my_list = ['https://www.flipkart.com/sherlock-holmes-complete-novels-stories-volume/p/itmf3jbqsvdktrsn?pid=9780553212419&lid=LSTBOK9780553212419QAWDRK&marketplace=FLIPKART&srno=s_1_13&otracker=AS_QueryStore_OrganicAutoSuggest_0_15&fm=SEARCH&iid=7fc6db9f-ec7d-42c1-b9e8-ba0fde32b658.9780553212419.SEARCH&ppt=Homepage&ppn=Homepage&ssid=esl8lszfjk0000001552810927096&qH=e717ef139779e544',
+#List of URLs of items on Flipkart.
+my_list = ['https://www.flipkart.com/samsung-galaxy-s9-plus-coral-blue-64-gb/p/itmf33a6qhrkyth3?pid=MOBF2VWVHDAZRQGH&srno=b_1_1&otracker=hp_omu_Top%2BOffers%2BOn%2BMobiles_3_7.dealCard.OMU_YZLF8ZJ051G3_7&otracker1=hp_omu_PINNED_neo%2Fmerchandising_Top%2BOffers%2BOn%2BMobiles_NA_dealCard_cc_3_NA_view-all_7&lid=LSTMOBF2VWVHDAZRQGHCF6TXA&fm=neo%2Fmerchandising&iid=6ef955c2-b372-4b94-92ca-3b6131ecb748.MOBF2VWVHDAZRQGH.SEARCH&ppt=browse&ppn=browse&ssid=gtpkjmcy9c0000001570971060174',
+           'https://www.flipkart.com/redmi-note-7s-astro-moonlight-white-32-gb/p/itm5e8bac3bcf4c4?pid=MOBFJFZDP9R3Z7YK&lid=LSTMOBFJFZDP9R3Z7YKQJSH2X&marketplace=FLIPKART&srno=b_1_1&otracker=hp_omu_Top%2BOffers_2_3.dealCard.OMU_88559FM66S42_3&otracker1=hp_omu_PINNED_neo%2Fmerchandising_Top%2BOffers_NA_dealCard_cc_2_NA_view-all_3&fm=neo%2Fmerchandising&iid=2af1c4be-4b89-454e-95ed-09de21c5962f.MOBFJFZDP9R3Z7YK.SEARCH&ppt=browse&ppn=browse&ssid=s8tyw8t4fk0000001570970986200',
+           'https://www.flipkart.com/sherlock-holmes-complete-novels-stories-volume/p/itmf3jbqsvdktrsn?pid=9780553212419&lid=LSTBOK9780553212419QAWDRK&marketplace=FLIPKART&srno=s_1_13&otracker=AS_QueryStore_OrganicAutoSuggest_0_15&fm=SEARCH&iid=7fc6db9f-ec7d-42c1-b9e8-ba0fde32b658.9780553212419.SEARCH&ppt=Homepage&ppn=Homepage&ssid=esl8lszfjk0000001552810927096&qH=e717ef139779e544',
            'https://www.flipkart.com/moto-z2-play-lunar-gray-64-gb/p/itmevt7xuk6xv3e7?pid=MOBEU9WRFZNAP2MJ&srno=s_1_4&otracker=search&lid=LSTMOBEU9WRFZNAP2MJVK3CZ5&fm=SEARCH&iid=a3be6899-5ef4-43a9-9dcd-ba4a059f63b1.MOBEU9WRFZNAP2MJ.SEARCH&ppt=SearchPage&ppn=Search&ssid=mmswc1u8cg0000001552811023594&qH=de88b155ee0c683f',
            'https://www.flipkart.com/highlander-men-s-checkered-casual-black-grey-shirt/p/itmfekrxkpxpdgg8?pid=SHTEZG79ZQGTFHZH&lid=LSTSHTEZG79ZQGTFHZHR9ENLK&marketplace=FLIPKART&srno=b_1_5&otracker=nmenu_sub_Men_0_Shirts&fm=neo%2Fmerchandising&iid=17c3cbcd-70f7-417c-b5a0-8453205cff2a.SHTEZG79ZQGTFHZH.SEARCH&ppt=StoreBrowse&ppn=Store&ssid=qi0y9l3wkw0000001552811061578',
            'https://www.flipkart.com/metronaut-men-solid-casual-blue-shirt/p/itmf5xtgj8h8syw7?pid=SHTF5XTGHYNPUED9&lid=LSTSHTF5XTGHYNPUED95AHKNN&marketplace=FLIPKART&srno=b_1_15&otracker=nmenu_sub_Men_0_Shirts&fm=neo%2Fmerchandising&iid=0ce9785f-4582-400d-a2a6-c62d2cb5296e.SHTF5XTGHYNPUED9.SEARCH&ppt=StoreBrowse&ppn=Store',
@@ -29,4 +33,20 @@ my_list = ['https://www.flipkart.com/sherlock-holmes-complete-novels-stories-vol
 
 for i in my_list:
     flipkart_price(i)
-  
+
+
+'''
+    url = 'https://www.flipkart.com/adidas-adiray-m-running-shoes-men/p/itmf7g8fxnxydsuc?pid=SHOF7G8FYPMJTE45&lid=LSTSHOF7G8FYPMJTE458RA8CT&marketplace=FLIPKART&spotlightTagId=TrendingId_osp%2Fcil&srno=b_1_1&otracker=hp_omu_Deals%2Bof%2Bthe%2BDay_1_ISKSFOA9MJUY_0&fm=neo%2Fmerchandising&iid=633f9421-e711-4aab-8d4b-900ac8af91f8.SHOF7G8FYPMJTE45.SEARCH&ppt=StoreBrowse&ppn=Store&ssid=kkwtb7mn5c0000001552810060372'
+
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+
+    getting the name of item
+    item_name = soup.find('p').text
+
+    getting the price
+    price = soup.find('div', class_ = '_1vC4OE _3qQ9m1').text
+
+    print("Current price of \"{}\" is: Rs. {}/-".format(item_name, price))
+
+'''
