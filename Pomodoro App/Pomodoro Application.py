157bfa2d51422074067aad27939b129e283f6afc
Ram Babu <ram.kottapally@gmail.com>
2019-10-15 17:00:16 +0530

    Pomodoro Application Basic

---
+++ Pomodoro App/Pomodoro Application.py
@@ -0,0 +1,21 @@
+#Pomodoro Application
+
+#Module to play audio files
+from playsound import playsound
+import time
+
+duration = int(input('Enter Duration for your Pomodoro{in minutes}: '))
+
+
+print(time.asctime())
+print('Pomodoro Started. Focus....')
+for i in range(10):
+    playsound('Tick-Tock.mp3')
+    #Giving 0.8 as argument works best for sleeping for 1second
+    time.sleep(0.8)
+
+print('Pomodoro Completed!')
+playsound('Alarm.mp3')
+
+
+
