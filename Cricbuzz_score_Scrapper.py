58ab16b066cb3fb494851dee0a27b6ddb1be119f
Ram Babu <ram.kottapally@gmail.com>
2020-03-08 13:47:46 +0530

    Score Scrapper
    Scrapes the live score and displays the data on the terminal. Details like Score and Batsmen details are shown.

---
+++ Cricbuzz_score_Scrapper.py
@@ -0,0 +1,42 @@
+import requests
+import bs4 as bs
+import time
+from prettytable import PrettyTable
+
+print(f"*********************** INDIA vs. AUSTRALIA Live Score ************************\n")
+
+def score_details():
+    url="https://www.cricbuzz.com/live-cricket-scores/22332/indw-vs-ausw-final-icc-womens-t20-world-cup-2020"
+
+    response = requests.get(url)
+    html = response.text
+    soup = bs.BeautifulSoup(html, 'lxml')
+
+    score = soup.find('div', class_= 'cb-min-bat-rw')
+    info = soup.findAll('div', class_= 'cb-min-itm-rw')
+
+
+
+    table = PrettyTable(['Batsman', 'Runs(Balls)', '4s', '6s', 'Strike Rate'])
+    for i in range(2):
+        batsman = info[i].select_one("div:nth-of-type(1)").text.strip()
+        runs = info[i].select_one("div:nth-of-type(2)").text.strip()
+        balls = info[i].select_one("div:nth-of-type(3)").text.strip()
+        fours = info[i].select_one("div:nth-of-type(4)").text.strip()
+        sixes = info[i].select_one("div:nth-of-type(5)").text.strip()
+        sr = info[i].select_one("div:nth-of-type(6)").text.strip()
+
+        table.add_row([batsman, runs + '(' + balls + ')', fours, sixes, sr])
+
+
+    print(f'Score: {score.text.strip()}\n')
+    print(table)
+    print(45*'+ ')
+
+
+'''Driver Code'''
+x = 0
+while x < 10:
+    score_details()
+    x += 1
+    time.sleep(30)
