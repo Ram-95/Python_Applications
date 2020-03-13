d7b610a6f8e316c2bc9062357e474d104deaf4fc
Ram Babu <ram.kottapally@gmail.com>
2020-03-13 13:49:29 +0530

    Also displays the filename
    Displays the filename that is currently executed.

--- Slack_Push_Notification.py
+++ Slack_Push_Notification.py
@@ -2,11 +2,12 @@
 ''' This script is used to get a Push Notification on Mobile using Slack.'''
 import slack
 import os
-def slack_message(message):
+def slack_message(message, filename):
     token = os.environ.get('SLACK_TOKEN')
     sc = slack.WebClient(token)
     sc.chat_postMessage(channel= 'python-notifications',
-                        text=message,
+                        text= '<< ' + os.path.basename(filename) + ' >>\n\n' + message,
                         username= 'Python-Script',
                         icon_emoji= ':sunglasses:')
 
+    print('\n***** SLACK Push Notification Successfully Sent. *****\n')
