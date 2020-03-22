226a83b4e263442ba6425c74c0732ce3ab8afeac
Ram Babu <ram.kottapally@gmail.com>
2020-03-22 23:04:14 +0530

    Add files via upload

--- Corona Tracker/COVID19_India_Information.py
+++ Corona Tracker/COVID19_India_Information.py
@@ -8,6 +8,8 @@ from prettytable import PrettyTable
 
 
 filename = 'COVID-19_India_Data.csv'
+state_filename = 'COVID-19_State_wise_Information.csv'
+
 today = date.today().strftime("%d-%b-%y")
 
 
@@ -16,11 +18,10 @@ response = requests.get(url)
 html = response.text
 soup = bs.BeautifulSoup(html, 'lxml')
 
-x = soup.find('ol').findAll('p')
+x = soup.find('div', class_= 'content newtab').find('p').text.strip()
 
 
-tbl = soup.find('table')
-t_items = tbl.findAll('tr')
+t_items = soup.find('div', class_= 'content newtab').find('table').find('tbody').findAll('tr')
 head = ['S.No', 'State/UT', 'Indian Cases', 'Foreign Cases', 'Discharged', 'Dead', 'Total Cases']
 
 
@@ -28,28 +29,44 @@ head = ['S.No', 'State/UT', 'Indian Cases', 'Foreign Cases', 'Discharged', 'Dead
 ind_table = PrettyTable(head)
 summ_table = PrettyTable(['Total', 'Indian Cases', 'Foreign Cases', 'Discharged', 'Dead', 'Total Cases'])
 
-for i in t_items[1:len(t_items)-1]:
-    data = []
+
+b = soup.findAll('div', class_='iblock_text')
+india_total = b[1].find('div').text.strip() + '(considering only Indian Citizens) = ' + b[1].find('span').text
+
+#Writing the state wise information to a file
+with open(state_filename, 'w') as f:
+    writer = csv.writer(f, delimiter= ',', lineterminator= '\n')
+    writer.writerow([x.upper() for x in head])
+
+
+for i in t_items[:len(t_items)-1]:
+    state_data = []
     for k in i.findAll('td'):
-        data.append(k.text.strip())
-    data.append(int(data[2]) + int(data[3]))
-    ind_table.add_row(data)
+        state_data.append(k.text.strip())
+    state_data.append(int(state_data[2]) + int(state_data[3]))
+    ind_table.add_row(state_data)
+
+    with open(state_filename, 'a') as f:
+        writer = csv.writer(f, delimiter=',', lineterminator='\n')
+        writer.writerow(state_data)
 
 
-print('\n' + '*'*20 + ' INDIA - STATE WISE INFORMATION ' + x[-1].text.strip() + ' ' + '*'*20 + '\n')
+print('\n' + '*'*20 + ' INDIA - STATE WISE INFORMATION ' + x + ' ' + '*'*20 + '\n')
 print(f'{ind_table}')
 
 summ = []
 for j in t_items[-1].findAll('td'):
     summ.append(j.text.strip())
 
-total_cases = int(summ[1]) + int(summ[2])
+total_cases = int(summ[1].replace('#', '')) + int(summ[2].replace('#', ''))
 summ.append(total_cases)
 
 summ_table.add_row(summ)
 
 print(f'\nSummary:\n{summ_table}')
-print(f'\n[OFFICIAL DATA] - {x[1].text.strip()}\n')
+print(f'\n[OFFICIAL DATA] - {india_total}\n')
+
+
 
 '''Code for writing to the Files.'''
 #Creating the CSV File if not already present
@@ -89,3 +106,5 @@ else:
         with open(filename, 'a') as f:
             writer = csv.writer(f, delimiter = ',', lineterminator = '\n')
             writer.writerow([today, total_cases, deaths])
+
+
